705bd46c6794c847a74c31ca6f5a6595e17606d8
FatalErrorX <fatalerrorxdevs@gmail.com>
2018-01-08 10:36:48 +0100

    init

---
+++ unity-controller/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "http://127.0.0.1:4001",
+      
+      ///
+      color: 'white'
+      ///
+      
+    };
+    this.socket = socketIOClient(this.state.endpoint)
+  }
+
+  // sending sockets
+  send = () => {
+    this.socket.emit('change color', this.state.color) // change 'red' to this.state.color
+  }
+
+  ///
+  
+  // adding the function
+  setColor = (color) => {
+    this.setState({ color })
+  }
+  
+  ///
+
+  render() {
+    // testing for socket connections
+
+    this.socket.on('change color', (col) => {
+      document.body.style.backgroundColor = col
+    })
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send() }>Change Color</button>
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
+      </div>
+    )
+  }
+}
+export default App;
